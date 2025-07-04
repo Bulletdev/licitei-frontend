96b81b544b6455b53db9e0bd7e859bb2ae34b05a
Michael Bullet <contato@michaelbullet.com>
2025-07-03 23:24:55 -0300

    Update licitacao.service.js

--- src/services/licitacao.service.js
+++ src/services/licitacao.service.js
@@ -3,7 +3,7 @@ import axios from "axios"
 class LicitacaoService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.VITE_API_BASE_URL || "https://licitei-backend-56315dca6f6b.herokuapp.com/api",
+      baseURL: import.meta.env.VITE_API_BASE_URL || "https://licitei-backend-56315dca6f6b.herokuapp.com/",
       timeout: 5000,
       headers: {
         "Content-Type": "application/json",
